bab4e0a856f7c9270a9093781620d0c5cd50c1f0
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2024-05-14 18:13:24 +0500

    fix bug of sidebar component

--- src/components/reusable/Sidebar/index.tsx
+++ src/components/reusable/Sidebar/index.tsx
@@ -8,7 +8,7 @@ type SideBarType = {
 const Sidebar: FC<SideBarType> = ({ className }) => {
   return (
     <div
-      className={`${className} transition-all duration-200 ease-linear overflow-hidden ml-5 rounded-t-md rounded-b-md bg-white dark:bg-main_grey`}
+      className={`${className} transition-all duration-200 ease-linear overflow-hidden ml-5 rounded-t-md rounded-b-md bg-max h-[80vh] overflow-y-scroll dark:bg-main_grey`}
     >
       <h6 className="text-center mt-2 font-semibold">Список нарушений </h6>
       <SidebarCard />
