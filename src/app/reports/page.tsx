e81fe00ac2bea7732cd485b98fd294167785c37b
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2024-05-29 17:00:36 +0500

    create Spinner component

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from "react";
 import ReportsCards from "@/components/reports/ReportsCards";
+import Spinner from "@/components/reusable/Spinner";
 
 const Reports: FC = () => {
   return (
     <div>
+      <Spinner />
       <h4 className="text-main_blue text-lg my-4 dark:text-white">Список нарушителей</h4>
       <div>
         <ReportsCards />
---
+++ src/components/reusable/Spinner/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+const Spinner = () => {
+  return (
+    <div className="orbit-spinner">
+      <div className="orbit"></div>
+      <div className="orbit"></div>
+      <div className="orbit"></div>
+    </div>
+  );
+};
+
+export default Spinner;
